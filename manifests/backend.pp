10d2cc402c222afb876ecb2a893dc1c4ea7c8fe5
Atha Kouroussis <akouroussis@gmail.com>
2011-04-16 23:06:22 -0300

    Added fronted and backend definitions

---
+++ manifests/backend.pp
@@ -0,0 +1,29 @@
+define haproxy::backend ($order = 30, $port = '80', $mode = 'http', $balance = 'roundrobin', 
+						$servers = '', $options = [], $hash_type = 'manual', $query = '',
+						$server_maxconn = '', $cookie = false, $cookie_name = '', $cookie_options = '',
+						$server_check = false, $check_inter = '', $check_fall = '', $appsession = false) {
+	# Some validations
+	case $hash_type {
+		'manual': {
+			# A servers array has to specified
+			if ( $servers == '' ) {
+				fail('servers can\'t empty if hash_type is manual')
+			}
+		}
+		'mcollective': {
+			# A mongo query has to be specified
+			if ( $query == '' ) {
+				fail('query can\'t empty if hash_type is mcollective')
+			}
+		}
+		default: {
+			fail("Invalid hash_type ${hash_type}. Accepted values are manual|mcollective")
+		}
+	}
+	
+	concat::fragment { "haproxy.cfg-backend-${name}":
+		target  => "${haproxy::params::configdir}/haproxy.cfg",
+		order   => $order,
+		content => template("haproxy/backend.cfg.erb")
+	}
+}
